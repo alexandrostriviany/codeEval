9b223a9d2d5dfb980600558f26f0cbf96d06769d
Oleksandr_Mosin@epam.com <Oleksandr_Mosin@epam.com>
2016-12-27 16:39:57 +0200

    Shortest Repetition added

--- src/com/codeEval/CleanUpTheWords.java
+++ src/com/codeEval/CleanUpTheWords.java
@@ -1,10 +1,5 @@
 package com.codeEval;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
 
 /**
  * Created by Oleksandr_Mosin on 12/27/2016.
@@ -12,12 +7,13 @@ import java.util.ListIterator;
 public class CleanUpTheWords {
     public static void runCleanUpTheWords(String line) {
         String clearText = "";
+        StringBuilder str = new StringBuilder();
         char[] notClearText = line.toCharArray();
         for (int i = 0; i<notClearText.length; i++){
             if (Character.isLetter(notClearText[i])) {
-                clearText += (notClearText[i]);
+                str.append(notClearText[i]);
                 if(i < notClearText.length-1 && !Character.isLetter(notClearText[i+1])){
-                    clearText += (" ");
+                    str.append(" ");
                 }
             }
         }
