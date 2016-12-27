bce180c0b9814dd3778fa6f00043bfa200363c35
Oleksandr_Mosin@epam.com <Oleksandr_Mosin@epam.com>
2016-12-27 16:33:19 +0200

    Shortest Repetition added

--- src/com/codeEval/CleanUpTheWords.java
+++ src/com/codeEval/CleanUpTheWords.java
@@ -12,16 +12,16 @@ import java.util.ListIterator;
 public class CleanUpTheWords {
     public static void runCleanUpTheWords(String line) {
         String clearText = "";
-        String[] notClearText = line.split("");
-        String symbols = "1234567890-=~!@#$%^&*()_+:'';\"\\|/_?.,<>{}[]`";
+        char[] notClearText = line.toCharArray();
         for (int i = 0; i<notClearText.length; i++){
-            if (!symbols.contains(notClearText[i])) {
-                clearText = clearText + (notClearText[i].toLowerCase());
-                if(i < notClearText.length-1 && symbols.contains(notClearText[i+1])){
-                    clearText = clearText + (" ");
+            if (Character.isLetter(notClearText[i])) {
+                clearText += (notClearText[i]);
+                if(i < notClearText.length-1 && !Character.isLetter(notClearText[i+1])){
+                    clearText += (" ");
                 }
             }
         }
-        System.out.println(clearText);
+        System.out.println(clearText.toLowerCase());
     }
 }
+
